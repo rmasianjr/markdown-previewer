201995d51fe3dd6230ab4aa17fa632daebae93ab
Ricardo Masian Jr <ricardo.masianjr@gmail.com>
2018-12-21 23:02:54 +0800

    Updates

---
+++ precache-manifest.8c7f84169db41d5393ac39e11278c040.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2c275135a06121482d1",
+    "url": "/markdown-previewer/static/js/runtime~main.b2c27513.js"
+  },
+  {
+    "revision": "969c7db985625620aee5",
+    "url": "/markdown-previewer/static/js/main.969c7db9.chunk.js"
+  },
+  {
+    "revision": "ebb01f1582699742545c",
+    "url": "/markdown-previewer/static/js/1.ebb01f15.chunk.js"
+  },
+  {
+    "revision": "969c7db985625620aee5",
+    "url": "/markdown-previewer/static/css/main.5ae9b894.chunk.css"
+  },
+  {
+    "revision": "471655c72a71070ba0caa8d752dc46be",
+    "url": "/markdown-previewer/index.html"
+  }
+];
