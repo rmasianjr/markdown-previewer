feef43a4b5692101c514c9e830db59601d37a210
Ricardo Masian Jr <ricardo.masianjr@gmail.com>
2018-12-21 22:48:59 +0800

    Configure marked function

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -2,12 +2,23 @@ import React from 'react';
 import marked from 'marked';
 import PropTypes from 'prop-types';
 
-const Preview = ({ markdownValue }) => (
-  <div
-    id="preview"
-    dangerouslySetInnerHTML={{ __html: marked(markdownValue) }}
-  />
-);
+const Preview = ({ markdownValue }) => {
+  const renderer = new marked.Renderer();
+  renderer.link = function(href, title, text) {
+    return `<a target="_blank" href=${href}>${text}</a>`;
+  };
+
+  marked.setOptions({
+    breaks: true
+  });
+
+  return (
+    <div
+      id="preview"
+      dangerouslySetInnerHTML={{ __html: marked(markdownValue, { renderer }) }}
+    />
+  );
+};
 
 Preview.propTypes = {
   markdownValue: PropTypes.string.isRequired
